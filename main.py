5b515e082e280bba832632a75817a58c92a66e6a
Kakani Krishna Sree <121270325+KrishKakani@users.noreply.github.com>
2023-01-02 18:54:07 -0600

    Random Password Generator

---
+++ main.py
@@ -0,0 +1,15 @@
+import random
+special_chars = ['$','@', '!', '*', '&','#','%']
+random.shuffle(special_chars)
+randompsd = ""
+for i in range(2):
+    randompsd += special_chars[i]
+    randompsd += chr(random.randint(65, 90))
+    randompsd += chr(random.randint(97, 122))
+    randompsd += str(random.randint(0, 9))
+r1 = []
+for i in range(len(randompsd)):
+    r1.append(randompsd[i])
+random.shuffle(r1)
+password = ''.join(r1)
+print(password)
